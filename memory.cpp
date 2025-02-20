3a486264bcc4dfc05d72edd16e41d5f50b7798a3
Kristiano0408 <cristianodigaspero@gmail.com>
2025-02-20 22:08:22 +0100

    A

--- memory.cpp
+++ memory.cpp
@@ -52,6 +52,25 @@ void Memory::writeDWord(uint64_t address, uint32_t value)
 
 };
 
+uint64_t Memory::readQWord(uint64_t address) const
+{
+    return  (static_cast<uint64_t>(data[address]) | (static_cast<uint64_t>(data[address + 1]) << 8) | (static_cast<uint64_t>(data[address + 2]) << 16) | (static_cast<uint64_t>(data[address + 3]) << 24) | (static_cast<uint64_t>(data[address + 4]) << 32) | (static_cast<uint64_t>(data[address + 5]) << 40) | (static_cast<uint64_t>(data[address + 6]) << 48) | (static_cast<uint64_t>(data[address + 7]) << 56));
+
+};
+
+void Memory::writeQWord(uint64_t address, uint64_t value)
+{
+    while (value > 0)
+    {
+        data[address] = value & 0xFF;
+        value >>= 8;
+        address++;
+    }
+
+};
+
+
+
 
 //clear the memory
 void Memory::clear()
--- prova.cpp
+++ prova.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "memory.hpp"
+#include <cstdint>
 
 using namespace std;
 
@@ -8,10 +9,14 @@ int main()
     Memory ram(1024); //create a memory with 1024 bytes
 
     //write some values in the memory
-    ram.writeByte(0, 0x12);
-    uint8_t value = ram.readByte(0);
+    ram.writeWord(0, 0x1234);
+    uint16_t value = ram.readWord(0);
     cout << "Value at address 0: " << hex << static_cast<int>(value) << endl;
-    
+
+    ram.writeDWord(2, 0x12345678);
+    uint32_t value2 = ram.readDWord(2);
+    cout << "Value at address 2: " << hex << static_cast<int>(value2) << endl;
+
 
 
 };
