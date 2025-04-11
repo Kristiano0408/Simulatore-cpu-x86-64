deab92aa34c3a0ad97613f999692b0a1e48cd361
Kristiano0408 <cristianodigaspero@gmail.com>
2025-04-11 19:57:42 +0200

    Update cacheManager.cpp

---
+++ cacheManager.cpp
@@ -0,0 +1,31 @@
+#include "cacheManager.hpp"
+#include <iostream>
+
+
+
+L1Cache::L1Cache(uint64_t size, uint64_t associativity)
+    : cacheSize(size), associativity(associativity), numSets(size / (associativity * CACHE_LINE_SIZE))
+{
+    //resizing the vector of sets
+    sets.resize(numSets);
+
+    for (uint64_t i = 0; i < numSets; ++i)
+    {
+        sets[i].lines.resize(associativity);
+        sets[i].setIndex = i;
+        for (uint64_t j = 0; j < associativity; ++j)
+        {
+            sets[i].lines[j].valid = false; // Initialize all lines as invalid
+            sets[i].lines[j].dirty = false; // Initialize all lines as not dirty
+            sets[i].lines[j].tag = 0;
+            sets[i].lines[j].lastAccessTime = 0; // Initialize last access time to 0
+        }
+    }
+}
+
+L1Cache::~L1Cache()
+{
+    // No dynamic memory to free, but can be used for cleanup if needed
+
+}
+
